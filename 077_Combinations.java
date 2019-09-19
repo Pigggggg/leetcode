df6c3ff560a58dbd6ef3a47397fc86030478fd3e
Pigggggg <brianlin.osu@gmail.com>
2019-09-18 22:51:55 -0700

    update 077
    077

--- 077_Combinations.java
+++ 077_Combinations.java
@@ -1,36 +1,19 @@
 class Solution {
-    private List<List<Integer>> out = null;
-	private Set<List<Integer>> set = null;
+    List<List<Integer>> out = new LinkedList<>();
     public List<List<Integer>> combine(int n, int k) {
-		out = new LinkedList<>();
-		set = new HashSet<>();
-		
-		for(int i = 1; i <= n; i++) {
-			LinkedList<Integer> tempArr = new LinkedList<>();
-			tempArr.add(i);
-			Set<Integer> tempSet = new HashSet<>();
-			tempSet.add(i);
-			dfs(n, k - 1, tempArr, tempSet, i);
-		}
-		
-		return out;
+        dfs(n, k, new LinkedList<Integer>(), 1);
+        return out;
     }
     
-    private void dfs(int n, int level, List<Integer> Arr, Set<Integer> curSet, int index) {
-		if(level == 0 && !set.contains(Arr)) {
-			set.add(Arr);
-			out.add(Arr);
-		}
-		
-		for(int i = index; i <= n; i++) {
-			if(!curSet.contains(i)) {
-				LinkedList<Integer> curArr = new LinkedList<>(Arr);
-				curArr.add(i);
-				Set<Integer> tempSet = new HashSet<>(curSet);
-				tempSet.add(i);
-				dfs(n, level - 1, curArr, tempSet, i + 1);
-			}
-		}
-	}
-    
+    private void dfs(int n, int level, List<Integer> arr, int index){
+        if(level == 0){
+            out.add(new LinkedList<Integer>(arr));
+        }
+        
+        for(int i = index; i <= n; i++){
+            arr.add(i);
+            dfs(n, level - 1, arr, i + 1);
+            arr.remove(arr.size() - 1);
+        }
+    }
 }
